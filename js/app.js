5d5708f9b5be9bacb20e92050ffcb2b243ec9ef7
aidangraff <graff@hartford.edu>
2022-10-27 10:03:34 -0400

    added javascript

---
+++ js/app.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    let rAB = document.getElementById(`rightArrowButton`);
+    let lAB = document.getElementById(`leftArrowButton`);
+    let img = document.getElementsByClassName(`imgSlide`);
+
+}
